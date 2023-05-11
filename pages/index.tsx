5697d2e8702608ae0e5a959ff5a1d5d08cb67f82
Hao Lee <haolee@xrex.io>
2023-05-11 17:47:33 +0800

    TEMP

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,20 @@ import { GetServerSideProps } from "next";
 import parser from "accept-language-parser";
 import Head from "next/head";
 
+const websiteurl = "https://ogdev-pearl.vercel.app";
+
 const en = {
   title: "Sign up with this code to get trading fee discount | XREX Exchange",
   description:
     "XREX exchange is operated by an internationally-recognized security team with 15+ years of experience. We provide the safest, cleanest and the most compliant trading environment. We offer smooth USD deposits and withdrawals in 120+ countries. Sign up with a referral code to get trading fee discount now!",
-  image: "https://shiabibi.vercel.app/discount0.png",
+  image: `${websiteurl}/discount0.png`,
 };
 
 const zh = {
   title: "XREX 好友推薦，立享交易折扣｜XREX 交易所",
   description:
     "XREX 交易所是由擁有 15 年以上經驗的資安團隊營運，提供最安全、最乾淨也最合規的加密貨幣交易體驗，美金存提款支援 120+ 個國家。現在就創建推薦碼邀請好友加入 XREX，分享交易手續費折扣！加入 XREX 俱樂部可以省更多交易手續費，揪好友一同加入 XREX 吧！",
-  image: "https://shiabibi.vercel.app/discount0zh_TW.png",
+  image: `${websiteurl}/discount0zh_TW.png`,
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -58,42 +60,75 @@ export default function Home({
     <>
       <Head>
         {/* <!-- COMMON TAGS --> */}
-        {/* <title key="title">{title}</title> */}
-        <title data-shuvi-head="true">{title}</title>
-        <meta name="keywords" content={description} data-shuvi-head="true" />
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <title data-shuvi-head="true">加密貨幣交易所推薦計畫｜幣安官方</title>
         <link rel="shortcut icon" href="favicon.ico" />
         <link rel="bookmark" href="favicon.ico" />
-
-
-        {/* <!-- Schema.org for Google --> */}
-        <meta itemProp="name" content={title} />
-        <meta itemProp="description" content={description} />
-        <meta itemProp="image" content={image} />
-
-        {/* <!-- Twitter --> */}
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image:src" content={image} />
-
-        {/* <!-- Open Graph general (Facebook, Pinterest & Google+) --> */}
-        <meta property="og:url" content={url} />
-        <meta property="og:image" content={image} />
-        <meta property="og:type" content="website" />
-        <meta property="og:site_name" content={description} />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:locale" content={locale} />
-
-        <meta key="robots" name="robots" content={"index,follow"} />
-        <meta key="googlebot" name="googlebot" content={"index,follow"}></meta>
-
+        <meta charSet="utf-8" />
         <meta
-          name="version"
-          content={process.env.NEXT_PUBLIC_APP_VERSION as string}
-        ></meta>
+          name="keywords"
+          content="幣安推薦、加密貨幣交易所推薦"
+          data-shuvi-head="true"
+        />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="官方推出邀請朋友的方式。您向 https://binance.com/Exchange 推薦的每位朋友都能讓您享有高達 40% 的佣金。邀請人數沒有上限！"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="og:url"
+          content="https://www.binance.com/zh-TC/activity/referral-entry/CPA?ref=CPA_005R34L6NF"
+          data-shuvi-head="true"
+        />
+        <meta property="og:type" content="website" data-shuvi-head="true" />
+        <meta
+          property="og:title"
+          content="加密貨幣交易所推薦計畫｜幣安官方"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="og:site_name"
+          content="Binance"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="og:image"
+          content="https://public.bnbstatic.com/images/referral-lite/cpa/zh-TW-ogImage.png"
+          data-shuvi-head="true"
+        />
+        {/* Twitter */}
+        <meta
+          property="twitter:title"
+          content="加密貨幣交易所推薦計畫｜幣安官方"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="twitter:site"
+          content="Binance"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="twitter:image"
+          content="https://public.bnbstatic.com/images/referral-lite/cpa/zh-TW-ogImage.png"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="twitter:image:src"
+          content="https://public.bnbstatic.com/images/referral-lite/cpa/zh-TW-ogImage.png"
+          data-shuvi-head="true"
+        />
+        <meta
+          property="twitter:card"
+          content="summary_large_image"
+          data-shuvi-head="true"
+        />
+        <link
+          rel="canonical"
+          href="https://www.binance.com/zh-TC/activity/referral-entry/CPA"
+          data-shuvi-head="true"
+        />
+
+        {/* Twitter */}
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
         OG Research - og
